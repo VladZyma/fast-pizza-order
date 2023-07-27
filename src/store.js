6d5ed68cc44dec950ee4f3be6b0935af26d9059f
Vladyslav Zyma <vladzima13@gmail.com>
2023-07-27 15:13:05 +0300

    added cartSlice

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import userReducer from './features/user/userSlice';
+import cartReducer from './features/cart/cartSlice';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  cart: cartReducer,
 });
 
 const store = configureStore({

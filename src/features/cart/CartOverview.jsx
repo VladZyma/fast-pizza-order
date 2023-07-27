2b25f4d1692213f7b73ba7ea0aeb71e6071a4fd5
Vladyslav Zyma <vladzima13@gmail.com>
2023-07-27 15:53:13 +0300

    added conditional rendering, cart total quantity and cart total price

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { getTotalCartQuantity, getTotalCartPrice } from './cartSlice';
+import { formatCurrency } from '../../utils/helpers';
 
 function CartOverview() {
+  const cartTotalQuantity = useSelector(getTotalCartQuantity);
+  const cartTotalPrice = useSelector(getTotalCartPrice);
+
+  if (!cartTotalQuantity) return null;
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>X pizzas</span>
-        <span>$XX.XX</span>
+        <span>{cartTotalQuantity} pizzas</span>
+        <span>{formatCurrency(cartTotalPrice)}</span>
       </p>
-      <Link to="/cart">OPEN CART</Link>
+      <Link to="/cart">OPEN CART &rarr;</Link>
     </div>
   );
 }

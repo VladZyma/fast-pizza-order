652ff3d4b37bb1cce689e531b27f75654edb2b77
Vladyslav Zyma <vladzima13@gmail.com>
2023-07-28 12:33:16 +0300

    added create order, get order, order list, search order

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -8,3 +8,47 @@ export async function getMenu() {
   const { data } = await res.json();
   return data;
 }
+
+export async function getOrder(id) {
+  const res = await fetch(`${API_URL}/order/${id}`);
+
+  if (!res.ok) throw new Error(`Couldn't find order #${id}`);
+
+  const { data } = await res.json();
+  return data;
+}
+
+export async function createOrder(newOrder) {
+  try {
+    const res = await fetch(`${API_URL}/order`, {
+      method: 'POST',
+      body: JSON.stringify(newOrder),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!res.ok) throw new Error();
+
+    const { data } = await res.json();
+    return data;
+  } catch (err) {
+    throw Error('Failed creating your order');
+  }
+}
+
+export async function updateOrder(id, updateObj) {
+  try {
+    const res = await fetch(`${API_URL}/order/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify(updateObj),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!res.ok) throw new Error();
+  } catch (err) {
+    throw Error('Failed updating your order');
+  }
+}
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,7 +6,7 @@ export function formatCurrency(value) {
 }
 
 export function formatDate(dateStr) {
-  return new Intl.formatDate('en', {
+  return new Intl.DateTimeFormat('en', {
     day: 'numeric',
     month: 'short',
     hour: '2-digit',
